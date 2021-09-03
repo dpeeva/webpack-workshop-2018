3fbe194698f22e8737e65df124d0c8c4891fa7cc
Desislava <desi.peeva@gmail.com>
2021-09-03 15:36:25 +0300

    test initial scripts

---
+++ src/button.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param {string} buttonName 
+ * @returns {Element}
+ */
+// take a string as button label and return an element
+const makeButton = (buttonName) => {
+    return `Button: ${buttonName}`
+}
+
+module.exports = makeButton
